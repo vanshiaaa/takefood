4a4b6f67f6d8229f3fd0b4f34b7cdd0ea07cf9f3
LiuXinhao <2779019980@qq.com>
2026-04-27 19:35:26 +0800

    员工分页查询：1pagehelper使用2.localdatetime的全局日期管理（在webmvcConfignration中扩展springmvc消息转换器)

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -19,4 +21,6 @@ public interface EmployeeMapper {
     //插入员工数据
     @Insert("insert into employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user) VALUES (#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser})")
     void insert(Employee employee);
+
+    Page<Employee> pageQuary(EmployeePageQueryDTO employeePageQueryDTO);
 }
