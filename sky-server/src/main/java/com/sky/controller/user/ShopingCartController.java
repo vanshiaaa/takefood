a906a56732ea82aa348dc85d80f49849c8bb8f7c
LiuXinhao <2779019980@qq.com>
2026-05-06 19:42:17 +0800

    购物车减商品（day7 作业）

--- sky-server/src/main/java/com/sky/controller/user/ShopingCartController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShopingCartController.java
@@ -37,4 +37,12 @@ public class ShopingCartController {
         return Result.success();
 
     }
+    //删除购物车中的一个商品
+    @PostMapping("/sub")
+    public Result sub(@RequestBody ShoppingCartDTO shoppingCartDTO){
+        log.info("删除购物车中一个商品：{}",shoppingCartDTO);
+        shoppingCartService.sub(shoppingCartDTO);
+        return Result.success();
+
+    }
 }
